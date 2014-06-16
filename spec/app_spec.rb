ccf41e23a5ebd05c7b7465a981b0a11f736eda0d
Alex Kleissner <hex337@gmail.com>
2014-06-15 17:45:57 -0700

    Updated all of the spec files to use present tense and the expect syntax, as per the betterspecs.org preference (http://betterspecs.org/#should)

--- spec/app_spec.rb
+++ spec/app_spec.rb
@@ -9,21 +9,21 @@ describe Sinatra::Application do
   end
 
   describe "when authentication is on" do
-    it 'should fail if no credentials are given' do
+    it 'fails when no credentials are given' do
       get '/'
-      last_response.should_not be_ok
+      expect(last_response).to_not be_ok
     end
 
-    it 'should fail if invalid credentials are given' do
+    it 'fails when invalid credentials are given' do
       authorize 'fub', 'buf'
       get '/'
-      last_response.should_not be_ok
+      expect(last_response).to_not be_ok
     end
 
-    it 'should success if valid credentials are given' do
+    it 'succeeds when valid credentials are given' do
       authorize 'foo', 'bar'
       get '/'
-      last_response.should be_ok
+      expect(last_response).to be_ok
     end
   end
 end
--- spec/check_scheduler_spec.rb
+++ spec/check_scheduler_spec.rb
@@ -4,16 +4,16 @@ describe CheckScheduler do
   let(:check) { Check.new(:name => 'WebCheck', :url => 'http://bark.meow', :frequency => 10) }
 
   describe 'run_check_if_its_time' do
-    it 'should run the check if the modulus is 0' do
-      CheckScheduler.send(:should_run_check, check, 0).should be_true
+    it 'runs the check if the modulus is 0' do
+      expect(CheckScheduler.send(:should_run_check, check, 0)).to be_true
     end
 
-    it 'should run the check if the modulus is 5' do
-      CheckScheduler.send(:should_run_check, check, 5).should be_false
+    it 'runs the check if the modulus is 5' do
+      expect(CheckScheduler.send(:should_run_check, check, 5)).to be_false
     end
 
-    it 'should run the check if the modulus is 10' do
-      CheckScheduler.send(:should_run_check, check, 10).should be_true
+    it 'runs the check if the modulus is 10' do
+      expect(CheckScheduler.send(:should_run_check, check, 10)).to be_true
     end
   end
 
