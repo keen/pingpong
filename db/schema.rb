872bd387ca6922fa0e04aca62a848e7f19ca4bb1
Alex Kleissner <alex@keen.io>
2015-02-12 15:55:51 -0800

    Added form validations to the new check page.

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150209143252) do
+ActiveRecord::Schema.define(version: 20150212151545) do
 
   create_table "checks", force: true do |t|
     t.string   "name"
