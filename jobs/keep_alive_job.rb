c38c6c8003006917a8cc373e9c20657b06b75802
Alex Kleissner <alex@keen.io>
2015-04-10 13:00:30 -0700

    This time the job doesn't crash things.

--- jobs/keep_alive_job.rb
+++ jobs/keep_alive_job.rb
@@ -5,14 +5,14 @@ require 'dotenv'
 Dotenv.load
 
 job do
-  every 5.minue
+  every 5.minute
 
   step 'ping instance' do
     if ENV['APP_NAME']
       app_name = ENV['APP_NAME']
 
       if ENV['HTTP_USERNAME']
-        app_name = "#{ENV['HTTP_USERNAME'}:#{ENV['HTTP_PASSWORD']}@#{app_name}"
+        app_name = "#{ENV['HTTP_USERNAME']}:#{ENV['HTTP_PASSWORD']}@#{app_name}"
       end
 
       url = "https://#{app_name}.herokuapp.com"
