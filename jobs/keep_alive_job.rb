0c25227e57af3509624572295d5a017321cb2727
Alex Kleissner <alex@keen.io>
2015-04-10 12:36:52 -0700

    Just wrap the job in case its not defined.

--- jobs/keep_alive_job.rb
+++ jobs/keep_alive_job.rb
@@ -8,13 +8,15 @@ job do
   every 5.minue
 
   step 'ping instance' do
-    app_name = ENV['APP_NAME']
+    if ENV['APP_NAME']
+      app_name = ENV['APP_NAME']
 
-    if ENV['HTTP_USERNAME']
-      app_name = "#{ENV['HTTP_USERNAME'}:#{ENV['HTTP_PASSWORD']}@#{app_name}"
+      if ENV['HTTP_USERNAME']
+        app_name = "#{ENV['HTTP_USERNAME'}:#{ENV['HTTP_PASSWORD']}@#{app_name}"
+      end
+
+      url = "https://#{app_name}.herokuapp.com"
+      HTTParty.get(url)
     end
-
-    url = "https://#{app_name}.herokuapp.com"
-    HTTParty.get(url)
   end
 end
